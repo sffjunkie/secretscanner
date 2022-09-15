bf7e9803d0337b90147784092a20d7bf52e56a4d
Simon Kennedy <sffjunkie+code@gmail.com>
2022-09-15 13:59:31 +0100

    Highlighter for secrets

---
+++ src/secretscanner/highlighter.py
@@ -0,0 +1,33 @@
+import re
+
+from rich.text import Text
+from rich.highlighter import RegexHighlighter
+
+user = r"[-0-9a-zA-Z$_+!`(),.?/;&=%#]+"
+password = r"(?P<yellow>[-0-9a-zA-Z$_+!`(),.?/;:&=%#]+)"
+
+userpass = fr"{user}:{password}@"
+
+URL_SECRET_RE = fr"[a-zA-Z]+[\w.]*://{userpass}[-0-9a-zA-Z$_+!`(),.?/;:&=%#]*"
+BEARER_SECRET_RE = r".*Bearer\s+(?P<yellow>\w+)"
+BOT_SECRET_RE = r".*Bot\s+(?P<yellow>\w+)"
+COLON_SECRET_RE = r"[\w.-]+\s*:\s*(?P<yellow>[\w.-]+)"
+EQUALS_SECRET_RE = r"[\w.-]+\s*=\s*(?P<yellow>[\w.-]+)"
+
+
+class SecretHighlighter(RegexHighlighter):
+    def highlight(self, text: Text):
+        for regex in [
+            URL_SECRET_RE,
+            BEARER_SECRET_RE,
+            BOT_SECRET_RE,
+            COLON_SECRET_RE,
+            EQUALS_SECRET_RE,
+        ]:
+            re_match = re.match(regex, str(text))
+            if re_match is not None:
+                text.highlight_regex(regex)
+                return
+
+        re_match = re.match(r"(?P<yellow>.+)", str(text))
+        text.highlight_regex(r"(?P<yellow>.+)")
